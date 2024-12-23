5b91860d74ffd01deeb45a91520786f4dc44413a
Oleksii Ukhalov <ukhalov.o.a@gmail.com>
2024-12-23 14:38:39 +0200

    Add a function from previous tasks

--- task4.js
+++ task4.js
@@ -1,7 +1,6 @@
 async function* generateNumbers(count) {
     for (let i = 0; i < count; i++) {
         const randomNum = Math.floor(Math.random() * 20);
-        console.log('Generated number:', randomNum);
         yield randomNum;
     }
 }
@@ -15,6 +14,36 @@ async function main() {
     }
 
     console.log('Array to search:', array);
+    try {
+        const index = await asyncFindIndex(array, searchValue);
+        console.log("Result:", index);
+    } catch (err) {
+        console.log(`Caught error: ${err.message}`);
+    }
 }
 
+const searchValue = 8;
 main();
+
+function asyncFindIndex(array, searchValue) {
+    return new Promise((resolve, reject) => {
+
+        if (!Array.isArray(array)) {
+            return reject(new TypeError('First argument must be an array'));
+        }
+        if (typeof searchValue !== 'number') {
+            return reject(new TypeError('Second argument must be a number'));
+        }
+
+        for (let i = 0; i < array.length; i++) {
+            setTimeout(() => {
+                if (array[i] === searchValue) {
+                    resolve(i);
+                }
+                if (i === array.length - 1) {
+                    resolve(-1);
+                }
+            }, 1500);
+        }
+    });
+}
